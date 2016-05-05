628007142f2cbe689fbc1db029041c554ec47bed
vsafronovici <Vitalie.Safronovici@endava.com>
2016-05-05 11:51:27 +0300

    Rss-feed plugin for Polopoly 10.16.0

---
+++ src/main/java/com/atex/plugins/rssfeed/Rssable.java
@@ -0,0 +1,21 @@
+package com.atex.plugins.rssfeed;
+
+import com.polopoly.cm.ContentId;
+import com.polopoly.siteengine.standard.feed.Feedable;
+
+public interface Rssable extends Feedable {
+    /**
+     * @return description of the feed entry
+     */
+    public String getItemDescription();
+
+    /**
+     * @return contentId for this content
+     */
+    public ContentId getItemContentId();
+
+    /**
+     * @return the path for the content
+     */
+    public ContentId[] getItemParentIds();
+}
